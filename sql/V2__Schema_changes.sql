13f35e50703ec0e6bd25fe3bcf9572a6545442db
Eric07110904 <azsx26735546@gmail.com>
2020-09-13 14:59:12 +0800

    change V2 schema

--- sql/V2__Schema_changes.sql
+++ sql/V2__Schema_changes.sql
@@ -2,35 +2,43 @@ DROP TABLE device_log;
 DROP TABLE tracking_device;
 DROP TABLE "user";
 
-CREATE Table users_table(
-    user_id varchar(36) PRIMARY KEY NOT NULL UNIQUE,
-    username varchar(15),
-    email varchar(50) NOT NULL UNIQUE,
-    phone varchar(10) NOT NULL UNIQUE,
-    password varchar(36) NOT NULL
+CREATE Table users(
+	id char(36) PRIMARY KEY NOT NULL UNIQUE,
+	username varchar(15) NOT NULL,
+	email varchar(50) NOT NULL UNIQUE,
+	phone varchar(10) NOT NULL,
+	password varchar(36) NOT NULL
 );
 
-CREATE Table trackers_table(
-    device_id varchar(36) PRIMARY KEY NOT NULL UNIQUE,
-    tkrname varchar(15) NOT NULL,
-    phone varchar(10) NOT NULL UNIQUE,
-    user_id varchar(36) NOT NULL UNIQUE,
-    FOREIGN KEY (user_id) REFERENCES users_table(user_id)
+CREATE Table trackers(
+	id char(36) PRIMARY KEY NOT NULL UNIQUE,
+	tkrname varchar(15) NOT NULL,
+	phone varchar(10) NOT NULL,
+	user_id char(36) NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE Table cage(
-    device_id varchar(36) NOT NULL UNIQUE,
-    FOREIGN KEY (device_id) references trackers_table(device_id),
-    alert_distance smallint
+CREATE Table boundary
+(
+	id CHAR(36) PRIMARY KEY NOT NULL UNIQUE,
+	tracker_id CHAR(36) NOT NULL,
+	FOREIGN KEY (tracker_id) references trackers(id),
+	time_start time,
+	time_end time,
+	weekday_start smallint,
+	weekday_end smallint,
+	lat float(24) NOT NULL,
+	lng float(24) NOT NULL,
+	radius smallint
 );
-
-CREATE Table tracker_requests(
-    request_id varchar(36) PRIMARY KEY NOT NULL UNIQUE,
-    issuer varchar(36),
-    trackerId varchar(36),
-    FOREIGN KEY (trackerId) references trackers_table(device_id),
-    delivery_time timestamptz,
-    received_time timestamptz,
-    request jsonb,
-    response jsonb
+CREATE Table tracker_requests
+(
+	request_id varchar(36) PRIMARY KEY NOT NULL UNIQUE,
+	issuer varchar(36),
+	trackerId varchar(36),
+	FOREIGN KEY (trackerId) references trackers(id),
+	delivery_time timestamptz,
+	received_time timestamptz,
+	request jsonb,
+	response jsonb
 );
